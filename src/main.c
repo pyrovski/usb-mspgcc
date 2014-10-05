6c92e984d974057c0d8bf5fb277cb804eb289e45
Peter Bailey <peter.eldridge.bailey@gmail.com>
2014-10-05 16:57:27 -0700

    add adc12 init

--- src/main.c
+++ src/main.c
@@ -35,7 +35,8 @@
 #include "usb_printf.h"
 
 //Indicates data has been received without an open rcv operation
-volatile BYTE bCDCDataReceived_event = FALSE;   
+volatile BYTE bCDCDataReceived_event = FALSE;
+volatile uint16_t last_conv;
 
 // Function declarations
 void msp_init(void);
@@ -72,14 +73,56 @@ int main(void) {
     }
 }
 
+/*
+#ifndef interrupt
+#define interrupt(x) void __attribute__((interrupt (x)))
+#endif
+*/
+#pragma vector=ADC12_VECTOR
+__interrupt void ADC_ISR(void){
+  uint16_t iv = ADC12IV;
+  if(iv & ADC12IV_ADC12IFG0){
+    // ADC12MEM0 result is ready
+    last_conv = ADC12MEM0;
+  }
+}
+
 /** Initializes modules and pins */
 void msp_init(void) {
     // Stop the watchdog
     wdt_stop();
+
     // Initialize USB interface
     usb_printf_init();
+
+    // configure ADC for temperature
+    // 1.5V reference
+    ADC12CTL0 = 
+      ADC12ON      | // turn on ADC
+      ADC12REFON   | // turn on ADC reference
+      ADC12MSC     | // automatic conversion restart
+      ADC12SHT0_12 ; // 1024 ADC12CLK cycles sample-and-hold
+    ADC12CTL1 = 
+      ADC12CONSEQ1     | // repeat single channel
+      ADC12DIV0        | 
+      ADC12DIV1        |
+      ADC12DIV2        | // divide ADC clock by 8
+      ADC12SHP         | // trigger sample-and-hold from timer
+      ADC12CSTARTADD_0 ; // store result in conversion address 0
+    // temperature sensor on
+    ADC12CTL2 = 
+      ADC12PDIV |  // predivide ADC clock by 4
+      ADC12RES1 ;  // 12-bit resolution
+    ADC12MCTL0 = 
+      ADC12INCH_10 | // select temperature diode
+      ADC12SREF_1  ; // VREF+/AVSS
+    ADC12IE = 
+      ADC12IE0 ; // enable interrupt on ADC12 mem 0 result
+
     // Enable global interrupts
     __enable_interrupt();
+
+    ADC12CTL0 |= ADC12ENC | ADC12SC;
 }
 
 /** Checks if there are events available to be processed
